f9f6789491b1401b3d1e7c8b8b87cf82c50796fc
harshit <harshityadav0927@gmail.com>
2024-07-13 13:58:13 +0530

    wrting file

--- files/server.js
+++ files/server.js
@@ -1,10 +1,15 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 
-fs.readFile('lorem.txt','utf8', (err, data)=>{
+fs.readFile(path.join(__dirname, 'lorem.txt'),'utf8', (err, data)=>{
     if(err) throw err;
     console.log(data);
 })
 
+fs.writeFile(path.join(__dirname, 'lorem2.txt'),'utf8gchgjvnhvhvjhhvjhvj', (err)=>{
+    if(err) throw err;
+    console.log('done');
+})
 
 
 // console.log("hello world")
